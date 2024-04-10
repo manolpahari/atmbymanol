a092c737a69ab2139bed996242cfd5b5fa6b6445
manolpahari <sharmamanol@hotmail.com>
2024-04-09 22:48:16 -0500

    adding more inputs and shortcuts to deposits page and extended primary button with button props

--- src/app/dashboard/deposit/page.tsx
+++ src/app/dashboard/deposit/page.tsx
@@ -8,30 +8,73 @@ function Page() {
       <div className="max-w-5xl items-center justify-center">
         <h1 className="text-2xl">Deposits</h1>
         <Spacing h="3" />
-        <form>
-          <div className="flex justify-between items-center gap-4">
-            <ButtonPrimary
-              buttonName="-"
-              type="button"
-              onClick={() => {
-                console.log('increment by 5');
-              }}
-            />
-            <input
-              className="text-md text-black bg-white p-2 rounded-md"
-              type="text"
-              name="depositAmount"
-              placeholder="$0"
-            />
-            <ButtonPrimary
-              buttonName="+"
-              type="button"
-              onClick={() => {
-                console.log('decrement by 5');
-              }}
-            />
+        {/* shortcuts buttons*/}
+        <div className="flex justify-between items-center gap-4 mt-2">
+          <ButtonPrimary
+            buttonName="$5"
+            type="button"
+            onClick={() => {
+              console.log('decrement by 5');
+            }}
+          />
+          <ButtonPrimary
+            buttonName="$10"
+            type="button"
+            onClick={() => {
+              console.log('decrement by 10');
+            }}
+          />
+          <ButtonPrimary
+            buttonName="$15"
+            type="button"
+            onClick={() => {
+              console.log('decrement by 15');
+            }}
+          />
+          <ButtonPrimary
+            buttonName="$20"
+            type="button"
+            onClick={() => {
+              console.log('decrement by 20');
+            }}
+          />
+        </div>
+        <Spacing h="3" />
+        <div className="flex justify-between items-start gap-4">
+          <ButtonPrimary
+            buttonName="-"
+            type="button"
+            onClick={() => {
+              console.log('increment by 5');
+            }}
+          />
+          {/* Form Input */}
+          <div>
+            <form className="flex flex-col items-center gap-4">
+              <input
+                className="text-md text-black bg-white p-2 rounded-md"
+                type="number"
+                name="depositAmount"
+                placeholder="$0"
+                disabled
+              />
+              <ButtonPrimary
+                buttonName="Complete Deposit"
+                type="submit"
+                onClick={() => {
+                  console.log('decrement by 5');
+                }}
+              />
+            </form>
           </div>
-        </form>
+          <ButtonPrimary
+            buttonName="+"
+            type="button"
+            onClick={() => {
+              console.log('decrement by 5');
+            }}
+          />
+        </div>
       </div>
     </section>
   );
